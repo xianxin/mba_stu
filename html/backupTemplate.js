55a209bdda34f8613b9c42d1ee7971f625c91667
xianxin <xianxingggg@163.com>
2014-10-11 09:29:02 +0800

    personal info v1.3

---
+++ html/backupTemplate.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>上海交通大学安泰经济与管理学院-MBA学生系统</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="js/public.js" ></script>
+</head>
+
+<body>
+<div class="hder">
+  <div class="hder_top">
+    <div class="hder_l">
+      <a href="javascript:;" title="上海交通大学安泰经济与管理学院-MBA学生系统" class="fl" style="margin-right:10px; display:inline;">
+        <img src="images/logo.png" width="303" height="48">
+      </a>
+      <span class="fl">学生系统</span>
+    </div>
+    <div class="hder_r">
+      <p id="head_right"></p>
+    </div>
+  </div>
+  <div class="hder_nav"><!--menu-start-->
+		<div class="nav" id="nav_id">
+			<script>$("#nav_id").load("/mbastu/_menu/mainMenu.html");</script>
+		</div>
+  </div><!--menu-end-->
+</div>
+
+<!--head-end-->
+<div id="main" tzreplacetextflg="main">
+<a href="TZ_CONTENT_SETUP_URL?tzspedivid=main&divheight=0&divwidth=0&tz_anchor=ac1&tz_speid=TZ_SEM_SPEDID" target="_self">设置分区</a>
+</div>
+<!--main-end-->
+<div class="footer1">
+	<div class="ft_box">
+      <div class="ft_box_l">
+        <div class="ft_list" id="ft_list">
+<script>$("#ft_list").load("/mbastu/footer.html");</script>
+         </div>
+      </div>
+      <div class="clear"></div>
+    </div>
+</div>
+</body>
+</html>
